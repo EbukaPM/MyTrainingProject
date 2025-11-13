a511d934cc32c0c141286b69445c7b68aeebf43a
Ebuka64 <ebuka0064@gmail.com>
2025-11-13 17:32:12 +0100

    implemented code

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -10,6 +10,6 @@ print("Your BMI Is: ",bmi)
 
 print("Check Your BMI Categories Below:")
 
-bmi_categories = ["Underweight: Below 18.5", "Healthy weight: 18.5 to 24.9", "Overweight: 25.0 to 29.9", "Obesity: 30.0 or greater"]
+bmi_categories = [" Underweight: Below 18.5", "Healthy weight: 18.5 to 24.9", "Overweight: 25.0 to 29.9", "Obesity: 30.0 or greater"]
 
 print(bmi_categories)
